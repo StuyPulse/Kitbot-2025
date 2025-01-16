1fa95c94ac77652f02edf2614e3f8e718e8cc4df
SP-COMPuter <se@stuypulse.com>
2025-01-15 19:32:53 -0500

    add camera offset

--- src/main/java/com/stuypulse/robot/constants/Cameras.java
+++ src/main/java/com/stuypulse/robot/constants/Cameras.java
@@ -1,12 +1,14 @@
 package com.stuypulse.robot.constants;
 
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.util.Units;
 
 /** This interface stores information about each camera. */
 public interface Cameras {
 
     public CameraInfo[] LimelightCameras = new CameraInfo[] {
-        new CameraInfo("limelight", new Pose3d())
+        new CameraInfo("limelight", new Pose3d(Settings.LENGTH / 2, 0, Units.inchesToMeters(16.5), new Rotation3d()))
     };
     
 
